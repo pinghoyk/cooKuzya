d54383f2638ca52150bff9e339d90efdaf2f2e06
pinghoyk <pinghoyk009@gmail.com>
2024-09-26 13:15:46 +0500

    добавление комментариев

--- parser.py
+++ parser.py
@@ -2,26 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-def recipes_povar(URL): # получение рецептов с 1 сайта
-	response = requests.get(URL)
+def recipes_povar(URL):  # Получение рецептов с 1 сайта
+    response = requests.get(URL)
 
-	recipes_dict = {}
+    recipes_dict = {} # Словарь для хранения данных
 
-	if response.status_code == 200:
-		soap = BeautifulSoup(response.text, "html.parser")
+    if response.status_code == 200:
+        soap = BeautifulSoup(response.text, "html.parser")
 
-		recipes_povar = soap.find("div", id="megaContainer") # весь контент на странице
-		recipe_container = recipes_povar.find("div", id="container")
-		recipe_wrap = recipe_container.find("div", id="mainWrapper")
-		recipe_area = recipe_wrap.find("div", class_="cont_area hrecipe")
+        # Получаем все необходимые блоки с контентом
+        recipes_povar = soap.find("div", id="megaContainer")
+        recipe_container = recipes_povar.find("div", id="container")
+        recipe_wrap = recipe_container.find("div", id="mainWrapper")
+        recipe_area = recipe_wrap.find("div", class_="cont_area hrecipe")
 
-		recipe_name = recipe_area.find("h1", class_="detailed").text.strip() # название рецепта
+        # Название рецепта
+        recipe_name = recipe_area.find("h1", class_="detailed").text.strip()
 
-		recipe_img_area = recipe_area.find("div", class_="bigImgBox")  # поиск изображения
-		recipe_link_img = recipe_img_area.find("a")
-		recipe_img = recipe_link_img.find("img", class_="photo") # здесь я обращаюсь к тегу img
-		img_src = recipe_img.get('src') # здесь получаю ссылку с тега
+        # Поиск изображения
+        recipe_img_area = recipe_area.find("div", class_="bigImgBox")
+        recipe_link_img = recipe_img_area.find("a")
+        recipe_img = recipe_link_img.find("img", class_="photo")
+        img_src = recipe_img.get('src')
 
+        # Ингредиенты
+        recipe_ingr = recipe_area.find("div", class_="ingredients_wrapper")
+        recipe_ingr_name = recipe_ingr.find("h2", class_="span").text.strip()
 
 		recipe_ingr = recipe_area.find("div", class_="ingredients_wrapper") # ингедиенты с ненужнойц хуйней
 		recipe_ingr_name = recipe_ingr.find("h2", class_="span").text.strip() # название состав и бла-бла
