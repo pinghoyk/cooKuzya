a31b0f9abd1ccb2f3e0111bf6b5e9ec4f1ae714f
pinghoyk <pinghoyk009@gmail.com>
2024-09-26 18:30:34 +0500

    вернула обратно

--- parser.py
+++ parser.py
@@ -2,103 +2,113 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-# Получение рецептов с сайта Povar
-def get_recipe_from_povar(url): 
-    response = requests.get(url)
+# Получение рецептов с сайта Povar.ru
+def recipes_povar(URL): 
+    response = requests.get(URL)
 
-    recipe_data = {}  # Словарь для хранения данных рецепта
+    recipes_dict = {} # Словарь для хранения данных
 
     if response.status_code == 200:
-        soup = BeautifulSoup(response.text, "html.parser")
+        soap = BeautifulSoup(response.text, "html.parser")
 
-        # Получаем блок с контентом рецепта
-        mega_container = soup.find("div", id="megaContainer")
-        container = mega_container.find("div", id="container")
-        main_wrapper = container.find("div", id="mainWrapper")
-        recipe_section = main_wrapper.find("div", class_="cont_area hrecipe")
+        # Получаем все необходимые блоки с контентом
+        recipes_povar = soap.find("div", id="megaContainer")
+        recipe_container = recipes_povar.find("div", id="container")
+        recipe_wrap = recipe_container.find("div", id="mainWrapper")
+        recipe_area = recipe_wrap.find("div", class_="cont_area hrecipe")
 
         # Название рецепта
-        recipe_title = recipe_section.find("h1", class_="detailed").text.strip()
+        recipe_name = recipe_area.find("h1", class_="detailed").text.strip()
 
-        # Изображение рецепта
-        image_section = recipe_section.find("div", class_="bigImgBox")
-        image_link = image_section.find("a")
-        image = image_link.find("img", class_="photo")
-        image_src = image.get('src')
+        # Поиск изображения
+        recipe_img_area = recipe_area.find("div", class_="bigImgBox")
+        recipe_link_img = recipe_img_area.find("a")
+        recipe_img = recipe_link_img.find("img", class_="photo")
+        img_src = recipe_img.get('src')
 
         # Ингредиенты
-        ingredients_wrapper = recipe_section.find("div", class_="ingredients_wrapper")
-        ingredients_title = ingredients_wrapper.find("h2", class_="span").text.strip()
+        recipe_ingr = recipe_area.find("div", class_="ingredients_wrapper")
+        recipe_ingr_name = recipe_ingr.find("h2", class_="span").text.strip()
 
-        # Список ингредиентов
-        ingredients_list = ingredients_wrapper.find("ul", class_="detailed_ingredients no_dots")
-        ingredients = []
+        # Получаем все ингредиенты
+        ingrs = recipe_ingr.find("ul", class_="detailed_ingredients no_dots")
+        ingr_list = []
 
-        # Находим все ингредиенты
-        ingredient_items = ingredients_list.find_all("li", class_="ingredient flex-dot-line")
-        for ingredient_item in ingredient_items:
-            ingredient_name = ingredient_item.find("span", class_="name").text.strip()
+        # Находим все <li> с ингредиентами
+        ingr_uls = ingrs.find_all("li", class_="ingredient flex-dot-line")
+        for ingr_ul in ingr_uls:
+            ingr_name = ingr_ul.find("span", class_="name").text.strip()  # Название ингредиента
 
-            # Проверка наличия значения и единицы измерения
-            ingredient_quantity = ingredient_item.find("span", class_="value")
-            ingredient_quantity = ingredient_quantity.text.strip() if ingredient_quantity else ""
+            # Проверяем наличие значения и единицы измерения
+            ingr_value = ingr_ul.find("span", class_="value")
+            ingr_value = ingr_value.text.strip() if ingr_value else ""
 
-            ingredient_unit = ingredient_item.find("span", class_="u-unit-name")
-            ingredient_unit = ingredient_unit.text.strip() if ingredient_unit else ""
+            ingr_unit = ingr_ul.find("span", class_="u-unit-name")
+            ingr_unit = ingr_unit.text.strip() if ingr_unit else ""
 
             # Добавляем ингредиент в список
-            ingredients.append({
-                "name": ingredient_name,
-                "quantity": ingredient_quantity,
-                "unit": ingredient_unit
+            ingr_list.append({
+                "name": ingr_name,
+                "quantity": ingr_value,
+                "unit": ingr_unit
             })
 
-        # Заголовок с "Как приготовить"
-        cook_instruction_title = recipe_section.find("h2", string=re.compile(r'Как приготовить')).text.strip()
+        # Поиск заголовка с названием "Как приготовить"
+        recipe_cook = recipe_area.find("h2", string=re.compile(r'Как приготовить')).text.strip()
 
-        instruction_section = recipe_section.find("div", class_="instructions")
-        instruction_steps = instruction_section.find_all("div", class_="instruction")
+        recipe_prepare = recipe_area.find("div", class_="instructions")
+        recipe_steps = recipe_prepare.find_all("div", class_="instruction")
 
-        # Список шагов с изображениями и описанием
+        # Список для хранения шагов с изображениями и текстом
         steps_info = []
 
         # Проход по всем шагам
-        for step in instruction_steps:
-            # Пытаемся найти изображение шага
-            step_image_section = step.find("div", class_="detailed_step_photo_big")
+        for step in recipe_steps:
+            # Пытаемся найти изображение
+            step_img = step.find("div", class_="detailed_step_photo_big")
             
-            if step_image_section:  # Если изображение есть
-                image_link = step_image_section.find("a", class_="stepphotos")
-                if image_link:
-                    step_image = image_link.find("img", class_="photo")
-                    step_image_src = step_image.get('src') if step_image else None
+            if step_img:  # Если изображение есть
+                img_link = step_img.find("a", class_="stepphotos")
+                if img_link:
+                    step_src = img_link.find("img", class_="photo")
+                    if step_src:
+                        step_link = step_src.get('src')
+                    else:
+                        step_link = None
                 else:
-                    step_image_src = None
+                    step_link = None
             else:
-                # Если изображения нет, находим номер шага
+                # Если изображений нет, ищем номер шага
                 step_number = step.find("div", class_="stepNumber")
-                step_image_src = f"Шаг {step_number.text.strip()}" if step_number else "Нет изображения и номера шага"
+                if step_number:
+                    step_link = f"Шаг {step_number.text.strip()}"
+                else:
+                    step_link = "Нет изображения и номера шага"
 
-            # Описание шага
-            step_description = step.find("div", class_="detailed_step_description_big").text.strip() if step.find("div", class_="detailed_step_description_big") else "Нет описания"
+            # Получаем текст шага
+            recipe_text = step.find("div", class_="detailed_step_description_big").text.strip() if step.find("div", class_="detailed_step_description_big") else "Нет описания"
 
             # Добавляем шаг в список
-            steps_info.append({"image_or_step": step_image_src, "description": step_description})
+            steps_info.append({"image_or_step": step_link, "text": recipe_text})
+
+
 
         # Собираем все данные в словарь
-        recipe_data = {
-            "recipe_name": recipe_title,
-            "image_url": image_src,
-            ingredients_title: ingredients,
-            "instructions_title": cook_instruction_title,
-            "steps": steps_info
+        recipes_dict = {
+            "recipe": recipe_name,
+            "img": img_src,
+            recipe_ingr_name : ingr_list,
+            "cook" : recipe_cook,
+            "steps_info" : steps_info
+
         }
 
     else:
-        recipe_data['error'] = f"Ошибка при запросе: {response.status_code}"
+        recipes_dict['Ошибка'] = f"Ошибка при запросе: {response.status_code}"
 
-    return recipe_data
+    return recipes_dict
 
 
-result = get_recipe_from_povar("https://povar.ru/recipes/kulebyaka_s_myasom-54393.html")
+result = recipes_povar("https://povar.ru/recipes/kulebyaka_s_myasom-54393.html")
 print(result)
+
