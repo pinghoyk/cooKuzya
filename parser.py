e392917c0b8650259046b3eec78cb2c5bda2c7c0
pinghoyk <pinghoyk009@gmail.com>
2024-09-26 14:49:21 +0500

    условие. при наличии фото выводит ссылку, если фото нет выводит блок с цифрами

--- parser.py
+++ parser.py
@@ -60,6 +60,38 @@ def recipes_povar(URL):  # Получение рецептов с 1 сайта
 
         # Список для хранения шагов с изображениями и текстом
         steps_info = []
+
+        # Проход по всем шагам
+        for step in recipe_steps:
+            # Пытаемся найти изображение
+            step_img = step.find("div", class_="detailed_step_photo_big")
+            
+            if step_img:  # Если изображение есть
+                img_link = step_img.find("a", class_="stepphotos")
+                if img_link:
+                    step_src = img_link.find("img", class_="photo")
+                    if step_src:
+                        step_link = step_src.get('src')
+                    else:
+                        step_link = None
+                else:
+                    step_link = None
+            else:
+                # Если изображений нет, ищем номер шага
+                step_number = step.find("div", class_="stepNumber")
+                if step_number:
+                    step_link = f"Шаг {step_number.text.strip()}"
+                else:
+                    step_link = "Нет изображения и номера шага"
+
+            # Получаем текст шага
+            recipe_text = step.find("div", class_="detailed_step_description_big").text.strip() if step.find("div", class_="detailed_step_description_big") else "Нет описания"
+
+            # Добавляем шаг в список
+            steps_info.append({"image_or_step": step_link, "text": recipe_text})
+
+
+
         # Собираем все данные в словарь
         recipes_dict = {
             "recipe": recipe_name,
