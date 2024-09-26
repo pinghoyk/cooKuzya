02dc75455d578962c41f333a6c8dbbbd6b9407e8
pinghoyk <pinghoyk009@gmail.com>
2024-09-26 12:57:51 +0500

    поиск рецепта

--- parser.py
+++ parser.py
@@ -14,3 +14,10 @@ def recipes_povar(URL): # получение рецептов с 1 сайта
 		recipe_container = recipes_povar.find("div", id="container")
 		recipe_wrap = recipe_container.find("div", id="mainWrapper")
 		recipe_area = recipe_wrap.find("div", class_="cont_area hrecipe")
+
+		recipe_name = recipe_area.find("h1", class_="detailed").text.strip() # название рецепта
+
+		recipe_img_area = recipe_area.find("div", class_="bigImgBox")  # поиск изображения
+		recipe_link_img = recipe_img_area.find("a")
+		recipe_img = recipe_link_img.find("img", class_="photo") # здесь я обращаюсь к тегу img
+		img_src = recipe_img.get('src') # здесь получаю ссылку с тега
