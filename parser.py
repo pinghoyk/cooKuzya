a1f97fca8d4da478d48e9b43efe1940e03fd9376
pinghoyk <pinghoyk009@gmail.com>
2024-09-26 12:58:22 +0500

    поиск одного ингредиента

--- parser.py
+++ parser.py
@@ -21,3 +21,13 @@ def recipes_povar(URL): # получение рецептов с 1 сайта
 		recipe_link_img = recipe_img_area.find("a")
 		recipe_img = recipe_link_img.find("img", class_="photo") # здесь я обращаюсь к тегу img
 		img_src = recipe_img.get('src') # здесь получаю ссылку с тега
+
+
+		recipe_ingr = recipe_area.find("div", class_="ingredients_wrapper") # ингедиенты с ненужнойц хуйней
+		recipe_ingr_name = recipe_ingr.find("h2", class_="span").text.strip() # название состав и бла-бла
+
+		ingr = recipe_ingr.find("ul", class_="detailed_ingredients no_dots") # список ингедиентов
+		ingr_ul = ingr.find("li", class_="ingredient flex-dot-line") # наименование ингредиентв
+		ingr_name = ingr_ul.find("span", class_="name").text.strip() # что написано, сколько и граммы
+		ingr_value = ingr_ul.find("span", class_="value").text.strip()
+		ingr_unit = ingr_ul.find("span", class_="u-unit-name").text.strip()
