ed7da7a1c659916a3cfb5262dfe451c0fdd3832c
pinghoyk <pinghoyk009@gmail.com>
2024-10-04 01:29:15 +0500

    создание бд

--- bot.py
+++ bot.py
@@ -24,3 +24,27 @@ buttons_main = [
 # Клавиатура
 keyboard_main = InlineKeyboardMarkup(row_width=2)
 keyboard_main.add(*buttons_main)  # Добавляем все кнопки за один раз
+
+
+def init_db():
+    with sqlite3.connect(DB_PATH) as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                id_col INTEGER PRIMARY KEY AUTOINCREMENT,
+                message INTEGER,
+                user_id INTEGER,
+                username TEXT,
+                first_name TEXT,
+                time_registration TIMESTAMP
+            )
+        """)
+    print(f"{LOG}База данных создана!")
+
+
+def SQL_request(request, params=()):
+    with sqlite3.connect(DB_PATH) as conn:
+        cursor = conn.cursor()
+        cursor.execute(request, params)
+        if request.strip().lower().startswith('select'):
+            return cursor.fetchone()
