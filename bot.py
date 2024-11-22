c0200fcf8eb37e309527daa8563815ac58874fd7
pinghoyk <pinghoyk009@gmail.com>
2024-11-22 19:39:45 +0500

    бд

--- bot.py
+++ bot.py
@@ -32,3 +32,38 @@ btn_back = InlineKeyboardButton(text=" ◀️ Назад", callback_data="btn_ba
 keyboard_main = InlineKeyboardMarkup(row_width=2).add(*buttons_main)
 keyboard_recipes = InlineKeyboardMarkup(row_width=1).add(*buttons_recipe)
 keyboard_markup = InlineKeyboardMarkup().add(btn_back)
+
+
+# ПРОВЕРКА
+if not os.path.exists(DB_NAME):
+    print(f"{LOG}База данных не найдена, создана новая!")
+try:
+    with sqlite3.connect(DB_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER,
+                message INTEGER,
+                username TEXT,
+                name TEXT,
+                time_registration TIMESTAMP
+            )
+        """)
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS local_recipes (
+                lr_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                recipe_name TEXT,
+                ingredients TEXT,
+                instructions TEXT,
+                id INTEGER,
+                is_filled INTEGER DEFAULT 0,  -- 0 - не заполнен, 1 - полностью заполнен
+                current_step INTEGER DEFAULT 0,  -- 0 - нет заполнения, 1 - заполнено название, 2 - ингредиенты и т.д.
+                FOREIGN KEY (id) REFERENCES users (id)
+            );
+        """)
+        
+        conn.commit()
+    print(f"{LOG}База данных успешно инициализирована!")
+except sqlite3.Error as e:
+    print(f"{LOG}Ошибка при работе с базой данных: {e}")
