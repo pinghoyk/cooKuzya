c7fecc02fde93b17ee2929f896cf7ad878562e90
pinghoyk <pinghoyk009@gmail.com>
2024-11-22 19:33:22 +0500

    глобальные переменные и клавиатуры

--- bot.py
+++ bot.py
@@ -6,3 +6,29 @@ import sqlite3
 import pytz
 import config
 import os
+
+bot = telebot.TeleBot(config.API)
+
+# Глобальные переменные
+DB_NAME = "cook.db"
+LOG = "Логи: "
+
+
+# Кнопки
+buttons_main = [
+    InlineKeyboardButton(text=" 📖 Мои рецепты", callback_data="my_recipe")
+]
+
+buttons_recipe = [
+    InlineKeyboardButton(text=" 🩷 Избранное", callback_data="favorite_recipe"),
+    InlineKeyboardButton(text=" 🗃 Личные рецепты", callback_data="create_recipe"),
+    InlineKeyboardButton(text=" ➕ Добавить рецепт", callback_data="add_recipe"),
+    InlineKeyboardButton(text=" ◀️ Назад", callback_data="back_recipe")
+]
+
+btn_back = InlineKeyboardButton(text=" ◀️ Назад", callback_data="btn_back")
+
+# Клавиатура
+keyboard_main = InlineKeyboardMarkup(row_width=2).add(*buttons_main)
+keyboard_recipes = InlineKeyboardMarkup(row_width=1).add(*buttons_recipe)
+keyboard_markup = InlineKeyboardMarkup().add(btn_back)
