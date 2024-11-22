29503daa6c8617bd96ef26e8aeaefb3c442d4e83
pinghoyk <pinghoyk009@gmail.com>
2024-11-23 00:25:50 +0500

    функции для добавления рецепта

--- bot.py
+++ bot.py
@@ -79,7 +79,6 @@ def SQL_request(request, params=(), fetchone=False):
             result = cursor.fetchone()
         else:
             result = cursor.fetchall()
-
         cursor.close()
         return result
 
@@ -100,3 +99,132 @@ def get_greeting(name):
 # Получение времени
 def now_time():
     return datetime.now(pytz.timezone('Asia/Yekaterinburg')).strftime("%Y-%m-%d %H:%M:%S")
+
+
+
+
+
+# Получение названия рецепта
+def handle_name(message, message_id, recipe_id=None, edit_mode=False):
+    user_id = message.chat.id
+    recipe_name = message.text.strip()
+
+    if recipe_name.lower() == '/start':
+        start(message)
+        return
+
+    if edit_mode:
+        SQL_request("UPDATE local_recipes SET recipe_name=? WHERE lr_id=?", (recipe_name, recipe_id))
+        current_step = SQL_request("SELECT current_step FROM local_recipes WHERE lr_id=?", (recipe_id,), fetchone=True)
+
+        if current_step:
+            current_step = current_step[0]
+            if current_step == 2:
+                bot.register_next_step_handler(message, handle_ingredients, message_id, recipe_id)
+            elif current_step == 3:
+                bot.register_next_step_handler(message, handle_steps, message_id, recipe_id)
+        return
+
+    recipe = SQL_request("SELECT lr_id, current_step FROM local_recipes WHERE id=? AND is_filled=0", (user_id,), fetchone=True)
+
+    if recipe:
+        # Если рецепт существует, обновляем его название и сбрасываем шаг на 1
+        recipe_id, current_step = recipe
+        SQL_request("UPDATE local_recipes SET recipe_name=?, current_step=1 WHERE lr_id=?", (recipe_name, recipe_id))
+    else:
+        # Если рецепта нет, создаем новый
+        SQL_request("INSERT INTO local_recipes (recipe_name, id, is_filled, current_step) VALUES (?, ?, 0, 1)",(recipe_name, user_id))
+        recipe_id = SQL_request("SELECT last_insert_rowid()", fetchone=True)[0]
+
+    markup = InlineKeyboardMarkup()
+    markup.add(InlineKeyboardButton(text="✍️ Перезаписать название", callback_data=f"change_name_{recipe_id}"))
+
+    bot.edit_message_text(text=f"Название: <b>{recipe_name}</b>\n\nКузя записывает состав:", chat_id=user_id, message_id=message_id, reply_markup=markup, parse_mode="HTML")
+
+    recipe_id = SQL_request("SELECT lr_id FROM local_recipes WHERE id=? AND is_filled=0", (user_id,), fetchone=True)[0]
+    bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
+    bot.register_next_step_handler(message, handle_ingredients, message_id, recipe_id)
+
+
+# Получение состава рецепта
+def handle_ingredients(message, message_id, recipe_id, edit_mode=False):
+    user_id = message.chat.id
+    ingredients = message.text.strip()
+
+    if ingredients.lower() == '/start':
+        start(message)
+        return
+
+    if edit_mode:
+        SQL_request("UPDATE local_recipes SET ingredients=? WHERE lr_id=?", (ingredients, recipe_id))
+        bot.register_next_step_handler(message, handle_steps, message_id, recipe_id)
+        return
+
+    recipe = SQL_request("SELECT lr_id, current_step FROM local_recipes WHERE lr_id=? AND is_filled=0", (recipe_id,), fetchone=True)
+
+    if recipe:
+        # Если рецепт найден, обновляем ингредиенты
+        SQL_request("UPDATE local_recipes SET ingredients=?, current_step=2 WHERE lr_id=?", (ingredients, recipe_id))
+
+        markup = InlineKeyboardMarkup()
+        markup.add(InlineKeyboardButton(text="✍️ Перезаписать состав", callback_data=f"change_ingredients_{recipe_id}"))
+
+        bot.edit_message_text(f"<b>Состав:</b> {ingredients}\n\nКузя готов записать шаги! Введите их, каждый на новой строке:", chat_id=user_id, message_id=message_id, reply_markup=markup,parse_mode="HTML")
+        bot.delete_message(message.chat.id, message.message_id)
+        bot.register_next_step_handler(message, handle_steps, message_id, recipe_id)
+
+
+# Получение шагов рецепта
+def handle_steps(message, message_id, recipe_id, edit_mode=False, attempt=1):
+    try:
+        user_id = message.chat.id
+        instructions = message.text.strip() if message.text else ""
+        steps = [step.strip() for step in instructions.split("\n") if step.strip()]
+
+        if instructions.lower() == '/start':
+            start(message)
+            return
+
+        # Проверяем на минимальное количество шагов и их корректность
+        if len(steps) < 2:
+            response_text = (
+                "Кузя ждет хотя бы два шага! Введите их снова (каждый шаг с новой строки):"
+                if attempt == 1 else
+                "Кузя снова напоминает: нужно хотя бы два шага! Попробуйте еще раз:"
+            )
+            msg = bot.edit_message_text(chat_id=user_id, message_id=message_id, text=response_text)
+            bot.delete_message(chat_id=user_id, message_id=message.message_id)
+            bot.register_next_step_handler(msg, handle_steps, message_id, recipe_id, edit_mode, attempt + 1)
+            return
+
+        formatted_instructions = "\n".join(steps)
+
+        if edit_mode:
+            # Обновляем шаги в базе данных
+            SQL_request("UPDATE local_recipes SET instructions=? WHERE lr_id=?", (formatted_instructions, recipe_id))
+
+            markup = InlineKeyboardMarkup(row_width=1)
+            markup.add(
+                InlineKeyboardButton(text="✍️ Перезаписать шаги", callback_data=f"change_steps_{recipe_id}"),
+                InlineKeyboardButton(text="📔 Показать рецепт", callback_data=f"show_recipe_{recipe_id}")
+            )
+
+            bot.edit_message_text(chat_id=user_id, message_id=message_id, text=f"<b>Шаги:</b>\n{formatted_instructions}", reply_markup=markup, parse_mode="HTML")
+        else:
+            # Добавляем новые шаги и помечаем рецепт как завершенный
+            SQL_request("UPDATE local_recipes SET instructions=?, is_filled=1, current_step=3 WHERE lr_id=?", (formatted_instructions, recipe_id))
+
+            markup = InlineKeyboardMarkup(row_width=1)
+            markup.add(
+                InlineKeyboardButton(text="✍️ Перезаписать шаги", callback_data=f"change_steps_{recipe_id}"),
+                InlineKeyboardButton(text="📔 Показать рецепт", callback_data=f"show_recipe_{recipe_id}")
+            )
+
+            bot.edit_message_text(chat_id=user_id, message_id=message_id, text=f"<b>Шаги:</b>\n{formatted_instructions}\n\nКузя вздохнул... рецепт писать больше не нужно было!", reply_markup=markup, parse_mode="HTML")
+
+        # Удаляем сообщение пользователя
+        if message and message.message_id:
+            bot.delete_message(chat_id=user_id, message_id=message.message_id)
+    except Exception as e:
+        print(f"Ошибка в handle_steps: {e}")
+
