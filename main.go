82b22f4f59b35f9e65a810c8d45317122cc89585
pinghoyk <pinghoyk009@gmail.com>
2025-07-05 16:38:20 +0500

    Добавлены структуры для работы с json

--- main.go
+++ main.go
@@ -10,3 +10,33 @@ import (
 	"github.com/joho/godotenv"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
+
+// Структуры для локализации
+type Locale struct {
+	Bot     BotLocale     `json:"bot"`
+	Buttons ButtonsLocale `json:"buttons"`
+	Commands CommandsLocale `json:"commands"`
+}
+
+type BotLocale struct {
+	Welcome string      `json:"welcome"`
+	Help    string      `json:"help"`
+	Save    SaveMessages `json:"save"`
+}
+
+type SaveMessages struct {
+	None string `json:"none"`
+	Yes  string `json:"yes"`
+}
+
+type ButtonsLocale struct {
+	Back     string `json:"back"`
+	Favorites string `json:"favorites"`
+	Delete   string `json:"delete"`
+	Read     string `json:"read"`
+}
+
+type CommandsLocale struct {
+	Start string `json:"start"`
+	Help  string `json:"help"`
+}
